0e0a318b02b68e9a7a178b6dfd38b370569ec644
cognivore <66186054+cognivore@users.noreply.github.com>
2023-02-08 21:56:51 +0000

    Initial commit

---
+++ lib/flats.ex
@@ -0,0 +1,18 @@
+defmodule Flats do
+  @moduledoc """
+  Documentation for `Flats`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Flats.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/flats_test.exs
@@ -0,0 +1,8 @@
+defmodule FlatsTest do
+  use ExUnit.Case
+  doctest Flats
+
+  test "greets the world" do
+    assert Flats.hello() == :world
+  end
+end
